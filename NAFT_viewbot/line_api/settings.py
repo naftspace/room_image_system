d5f7283788cb6685768874b434127ead236b3fa0
NAFT <naft.space@gmail.com>
2018-04-01 22:57:33 +0900

    Update settings.py

--- NAFT_viewbot/line_api/settings.py
+++ NAFT_viewbot/line_api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uh6#rix*3736tzm4*rze+60o*#dt&f@(n=u4yfh8$(aw+=_-es'
+SECRET_KEY = 'Enter Django secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
